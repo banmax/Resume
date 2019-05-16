54f46069256635f73c0809a3ae7e056190ded263
banmax <we21weimingliang@qq.com>
2019-05-16 17:32:50 +0800

    使用combineReducers完成对数据的拆分管理

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group'
+import * as actionCreators from './store/actionCreators';
 import {
   HeaderWrapper,
   Logo,
@@ -51,23 +52,17 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.focused
+    focused: state.header.focused
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     handleInputFocus() {
-      const action = {
-        type: 'search_focus'
-      };
-      dispatch(action);
+      dispatch(actionCreators.searchFocus());
     },
     handleInputBlur() {
-      const action = {
-        type: 'search_blur'
-      };
-      dispatch(action);
+      dispatch(actionCreators.searchBlur());
     }
   }
 }
